046f55272ea7000013c98a9c9e0d749e82d9118a
daejjyu <shsh95kr@naver.com>
2020-05-06 23:53:20 +0900

    분할 _ 곱셈

---
+++ BOJ/1629.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+int A, B, C;
+
+int power(int n, int k)
+{
+	if (k == 0) return 1;
+
+	int temp = power(n, k / 2);
+	int result = 1LL * temp * temp % C;
+	if (k % 2)
+		result = 1LL * result*n%C;
+	return result;
+}
+
+int main()
+{
+	scanf("%d %d %d", &A, &B, &C);
+
+	printf("%d", power(A, B));
+}
