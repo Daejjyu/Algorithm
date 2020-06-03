42e96b4202aa8bedaf9fa0aaee574457a755a7b9
daejjyu <shsh95kr@naver.com>
2020-06-03 15:34:45 +0900

    OX퀴즈

--- BOJ/2751_2.cpp
+++ BOJ/2751_2.cpp
@@ -1,42 +1,79 @@
 #include <iostream>
-using namespace std;
 
-int arr[1000000];
+const int MAX = 1e6 + 1;
+int arr[MAX];
+int temp[MAX];
 
-void quickSort(int start, int end)
+void quickSort(int s, int e)
 {
-	if (start >= end) return;
+	if (s < e) {
+		int pivot = arr[(s + e) / 2];
+		int left = s;
+		int right = e;
 
-	int left = start;
-	int right = end;
-	int pivot = arr[start + (end - start) / 2];
+		while (left < right) {
+			while (arr[left] < pivot)
+				left++;
+			while (arr[right] > pivot)
+				right--;
+			if (left <= right)
+				std::swap(arr[left++], arr[right--]);
+		}
 
-	while (left <= right) {
-		while (arr[left] < pivot)
-			left++;
-		while (arr[right] > pivot)
-			right--;
-		if (left <= right)
-			swap(arr[left++], arr[right--]);
+		quickSort(s, right);
+		quickSort(left, e);
 	}
-
-	quickSort(start, right);
-	quickSort(left, end);
 }
 
-int main(void)
+void merge(int s, int e)
 {
-	int n, d;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-	{
-		scanf("%d", &d);
-		arr[i] = d;
-	}
-	quickSort(0, n - 1);
+	int mid = (s + e) / 2;
+	int index1 = s;
+	int index2 = mid + 1;
+	int indexTemp = s;
 
-	for (int i = 0; i < n; i++)
+	while (index1 <= mid && index2 <= e) {
+		if (arr[index1] < arr[index2])
+			temp[indexTemp++] = arr[index1++];
+		else
+			temp[indexTemp++] = arr[index2++];
+	}
+
+	while (index1 <= mid)
+		temp[indexTemp++] = arr[index1++];
+	while (index2 <= e)
+		temp[indexTemp++] = arr[index2++];
+
+	for (int i = s; i <= e; i++)
 	{
-		printf("%d \n", arr[i]);
+		arr[i] = temp[i];
+	}
+}
+
+void mergeSort(int s, int e)
+{
+	if (s < e) {
+		int mid = (s + e) / 2;
+		mergeSort(s, mid);
+		mergeSort(mid + 1, e);
+		merge(s, e);
+	}
+}
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d", arr + i);
+	}
+
+	//mergeSort(0, N - 1);
+	quickSort(0, N - 1);
+
+	for (int i = 0; i < N; i++)
+	{
+		printf("%d\n", arr[i]);
 	}
 }
