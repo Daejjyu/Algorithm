fe005afef798bc248871c7824f72f108e3adf34c
daejjyu <shsh95kr@naver.com>
2020-06-11 23:33:59 +0900

    bfs_탈출

--- BOJ/16397.cpp
+++ BOJ/16397.cpp
@@ -1,69 +1,63 @@
 #include <iostream>
 #include <queue>
+#include <string>
 using namespace std;
 
-const int MAX = 100000;
-int visited[MAX];
+queue<int> q;
+int visited[100000];
 
-int main(void)
+bool possible = false;
+int ans = 0;
+
+int N, T, G;
+void bfs(int start)
 {
-	int N, T, G;
-	scanf("%d %d %d", &N, &T, &G);
-
-	queue<int> q;
-	q.push(N);
-
-	int t = -1;
-	bool success = false;
-
-	while (!success & !q.empty()) {
-		int qSize = q.size();
-		while (qSize--) {
-			int pos = q.front();
+	q.push(start);
+	visited[start] = 1;
+	while (!q.empty()) {
+		int curSize = q.size();
+		while (curSize--) {
+			int cur = q.front();
 			q.pop();
 
-			if (pos == G) {
-				success = true;
-				break;
+			if (cur == G) {
+				possible = true;
+				return;
 			}
 
-			int buttonBNum;
-			if (pos == 0)
-				buttonBNum = 0;
-			else {
-				int firsitDigit = pos * 2;
-				int digit = 1;
-				while (firsitDigit >= 10) {
-					firsitDigit /= 10;
-					digit *= 10;
+			int next1 = cur + 1;
+			if (next1 <= 99999) {
+				if (!visited[next1]) {
+					q.push(next1);
+					visited[next1] = 1;
 				}
-				buttonBNum = pos * 2 - digit;
 			}
-			int nextP[2] = { pos + 1,buttonBNum };
+			int next2 = cur * 2;
 
-			for (int i = 0; i < 2; i++)
-			{
-				int next = nextP[i];
-				if (i == 1)
-					if (pos * 2 > MAX - 1)
-						continue;
-				if (next < 0 || MAX - 1 < next)
-					continue;
-				if (visited[next])
-					continue;
-				visited[next]++;
-				q.push(next);
+			if (next2 != 0 && next2 <= 99999) {
+				string s = to_string(next2);
+				s[0] -= 1;
+				next2 = stoi(s);
+				if (!visited[next2]) {
+					q.push(next2);
+					visited[next2] = 1;
+				}
 			}
 		}
-		t++;
-		if (t > T)
-			break;
+		ans++;
+		if (ans > T)
+			return;
 	}
+}
 
-	if (t < 0)
-		t = 0;
-	if (success && t <= T)
-		printf("%d", t);
+int main()
+{
+	scanf("%d %d %d", &N, &T, &G);
+
+	bfs(N);
+
+	if (possible)
+		printf("%d", ans);
 	else
 		printf("ANG");
 }
