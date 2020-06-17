ec6b1e66fd8006ae106a8916f2e4eab14910d8b8
daejjyu <shsh95kr@naver.com>
2020-06-17 16:21:44 +0900

    복습

---
+++ BOJ/11729_2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+int N;
+
+void Hanoi(int num, int from, int left, int to)
+{
+	if (num == 1) {
+		printf("%d %d\n", from, to);
+	} else {
+		Hanoi(num - 1, from, to, left);
+		Hanoi(1, from, left, to);
+		Hanoi(num - 1, left, from, to);
+	}
+}
+
+int main()
+{
+	scanf("%d", &N);
+
+	printf("%d\n", (1 << N) - 1);
+
+	Hanoi(N, 1, 2, 3);
+}
