9cf2afb472ac366b4a7d02d35d0a91ff3abcd4ed
daejjyu <shsh95kr@naver.com>
2020-12-14 18:56:35 +0900

    solved ~37

---
+++ Jungle/Week1/28_9663.py
@@ -0,0 +1,17 @@
+N = int(input())
+
+col = [0]*N+1
+
+ans = 0
+
+
+def nQueen(i):
+    if(1):
+        if(i == N):
+            ans += 1
+        else:
+            for(i in )
+
+
+nQueen(0)
+print(ans)
