07c32691cab4e558f6c34ff3822d3c65fe87fac7
daejjyu <shsh95kr@naver.com>
2020-12-19 12:11:26 +0900

    곱셈

--- Jungle/Week2/08_1629_곱셈.py
+++ Jungle/Week2/08_1629_곱셈.py
@@ -3,21 +3,17 @@ sys.setrecursionlimit(50000000)
 
 A, B, C = map(int, input().split())
 
+ans=0
 
-def Remainder(cnt):
-    if(cnt%2):
-        
-    print('in')
-    if(cnt == 1):
-        ans = 1
-    if(cnt>1):
-        temp = Remainder(int(cnt/2))
-    if(cnt==1):
-        temp = 1
-    ans = (temp * temp) % C
-    if(cnt % 2):
-        print(ans, A, C)
-        ans = (ans * A) % C
+def remainder(cnt):
+    if(cnt<=0):
+        return 1
+    ret=0
+    temp = remainder(int(cnt/2))
+    global ans
+    ans = temp * temp % C
+    if(cnt%2): #홀수이면
+        ans = (ans*A)%C
     return ans
 
-print(Remainder(B))
+print(remainder(B))
