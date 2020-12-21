6fecc5f2b62bcf0fdc4e11937a669a798addad63
daejjyu <shsh95kr@naver.com>
2020-12-21 17:06:27 +0900

    solved

--- Jungle/Week2/19_2812_크게 만들기.py
+++ Jungle/Week2/19_2812_크게 만들기.py
@@ -1,7 +1,14 @@
+from collections import deque
 N, K  = map(int,input().split())
 num = list(str(input()))
-num.sort()
-for i in range(N-1,K-1,-1):
-    print(num[i],end='')
-if(N-1==K-1):
-    print(0)
+
+que = deque()
+
+for i in num:
+    while(K and que and que[-1]<i):
+        que.pop()
+        K-=1
+    que.append(i)
+
+for i in range(len(que)-K):
+    print(que[i],end='')
