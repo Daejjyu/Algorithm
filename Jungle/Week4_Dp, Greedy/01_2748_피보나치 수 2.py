d5800c486d06b212f848abaac73f15f5eee5d433
SeungHyun Kim <daejjyu@gmail.com>
2021-01-02 15:10:46 +0900

    solved

--- Jungle/Week4_Dp, Greedy/01_2748_피보나치 수 2.py
+++ Jungle/Week4_Dp, Greedy/01_2748_피보나치 수 2.py
@@ -1,11 +1,17 @@
 import sys
 
-input = sys.stdin.readline
 
-N = int(input())
-dp = [0] * (N + 1)
-dp[1] = 1
-dp[2] = 1
-for i in range(3, N + 1):
-    dp[i] = dp[i - 1] + dp[i - 2]
-print(dp[N])
+def solve():
+    input = sys.stdin.readline
+
+    N = int(input())
+    dp = [0] * (N + 1)
+    dp[1] = 1
+    if (N > 1):
+        dp[2] = 1
+    for i in range(3, N + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    print(dp[N])
+
+
+solve()
