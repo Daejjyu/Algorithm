dbfc89559d830dcd190db63cc928f907dd4a0b3c
SeungHyun Kim <daejjyu@gmail.com>
2021-01-07 11:13:39 +0900

    solved

--- Jungle/Week4_Dp, Greedy/08_11047_동전 0.py
+++ Jungle/Week4_Dp, Greedy/08_11047_동전 0.py
@@ -1,18 +1,18 @@
 import sys
 
+sys.setrecursionlimit(50000)
+
 
 def solve():
-    input = sys.stdin.readline
-
     N, K = map(int, input().split())
     coin = [int(input()) for _ in range(N)]
+    coin.reverse()
 
     ans = 0
-    for i in range(len(coin) - 1, -1, -1):
-        ans += (int(K / coin[i]))
-        K = int(K % coin[i])
-        if (K == 0):
-            break
+    for i in coin:
+        if (K / i):
+            ans += int(K / i)
+        K %= i
     print(ans)
 
 
--- Jungle/Week4_Dp, Greedy/10_1931_회의실배정.py
+++ Jungle/Week4_Dp, Greedy/10_1931_회의실배정.py
@@ -8,12 +8,8 @@ def solve():
 
     list1 = [list(map(int, input().split())) for _ in range(N)]
     list1.sort(key=lambda x: (x[1], x[0]))
-    end, ans = 0, 0
-    for s, e in list1:
-        if (s >= end):
-            ans += 1
-            end = e
-    print(ans)
 
+
+20
 
 solve()
