f982ea9ae1040a687323fb335c098297e32350c3
SeungHyun Kim <daejjyu@gmail.com>
2021-05-13 15:57:05 +0900

    solved

--- Programmers/pro_42578_위장.py
+++ Programmers/pro_42578_위장.py
@@ -1,6 +1,7 @@
 def solution(clothes):
     from collections import Counter
     from functools import reduce
+    
     cnt = Counter([kind for name, kind in clothes])
     answer = reduce(lambda acc,cur: acc *(cur+1),cnt.values(),1)-1
     return answer
