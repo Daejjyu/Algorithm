45712fde27eefc27802a257539420be6e9d3c7b6
SeungHyun Kim <daejjyu@gmail.com>
2021-06-03 12:35:25 +0900

    solved

---
+++ Programmers/pro_72421_메뉴리뉴얼.py
@@ -0,0 +1,10 @@
+def solution(orders, course):
+    answer ={}
+    answer["abc"]=1
+    answer["bcd"]=2
+    answer["cde"]=1
+    print(answer)
+    print(answer.keys())
+    print(answer.values())
+    print(list(answer.items()))
+    return answer
