63baaebaa1be42b54c8a5922fd1051c89f2f0bd0
SeungHyun Kim <daejjyu@gmail.com>
2021-06-19 16:30:34 +0900

    solved

---
+++ Programmers/pro_72410_신규아이디추천.js
@@ -0,0 +1,11 @@
+function solution(new_id) {
+    const answer = new_id
+        .toLowerCase() // 1
+        .replace(/[^\w-_.]/g, '') // 2
+        .replace(/\.+/g, '.') // 3
+        .replace(/^\.|\.$/g, '') // 4
+        .replace(/^$/, 'a') // 5
+        .slice(0, 15).replace(/\.$/, ''); // 6
+    const len = answer.length;
+    return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3 - len);
+}
