26bfcce208e0ff222d97e3445198b7967fece480
SeungHyun Kim <daejjyu@gmail.com>
2021-06-25 00:28:07 +0900

    solved

--- Programmers/pro_60062_외벽점검.py
+++ Programmers/pro_60062_외벽점검.py
@@ -1,25 +1,20 @@
-# 파이썬
+
 from itertools import permutations
 def solution(n, weak, dist):
-    L = len(weak)
+    l = len(weak)
+    answer = 0
     cand = []
-    weak_point = weak + [w+n for w in weak]  # 선형으로
-
-    for start in enumerate(weak):
-        for friends in permutations(dist):  # 순열 이용
-            count = 1
-            position = start
-            # 친구 조합 배치
-            for friend in friends:
-                position += friend
-                # 끝 포인트까지 도달 못했을 때
-                if position < weak_point[i+L-1]:
-                    count += 1  # 친구 더 투입
-                    # 현재 위치보다 멀리 있는 취약지점 중 가장 가까운 위치로
-                    position = [w for w in weak_point[i+1:i+L]
-                                if w > position][0]
-                else:  # 끝 포인트까지 도달
-                    cand.append(count)
+    weakExtend = weak + [n+x for x in weak]    
+    for i, start in enumerate(weak):
+        for distArr in permutations(dist):
+            pos = start
+            cnt =1
+            for num in distArr:
+                pos+=num
+                if pos<weakExtend[i+l-1]:
+                    cnt+=1
+                    pos = [x for x in weakExtend[i+1:i+l] if x>pos][0]
+                else:
+                    cand.append(cnt)
                     break
-
     return min(cand) if cand else -1
